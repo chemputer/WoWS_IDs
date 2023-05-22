99dfecdc62a4a711e751a26dbb905435da33f03c
Chemputer <46601333+chemputer@users.noreply.github.com>
2023-05-22 19:26:16 -0400

    Massively overhauled the formatting and added comments and docstrings.

--- WoWSInductions.py
+++ WoWSInductions.py
@@ -1,11 +1,15 @@
 import PySimpleGUI as sg
 from wowspy import Wows
+# pyperclip is needed to copy the ID to the clipboard
 import pyperclip
+# this is simply config.py file that it's drawing the API key from so that I don't have to make that public
 from config import APIKEY
+# allows for caching of API lookups
+from functools import lru_cache
 
 
 
-# create an instance of the WoWS API with my API Key
+# create an instance of the WoWS API with my API Key from the config file
 my_api=Wows(APIKEY)
 
 # just putting all the regions even though we only use NA and EU.
@@ -16,8 +20,7 @@ RU = my_api.region.RU
 
 
 
-# Add some color
-# to the window
+# theme for the SimpleGUI windows
 sg.theme('Default1')     
 
 # Very basic window.
@@ -28,29 +31,85 @@ layout = [
     [sg.Text('IGN', size =(15, 1)), sg.InputText()],
     [sg.Submit(), sg.Cancel()]
 ]
+# title of the first window
 window = sg.Window('WoWS IGN to Player ID', layout)
+
+# properly handles closing the window upon submission or clicking "x".
 while True:
     event, values = window.read()
     if event in (None, 'Close Window'): # if user closes window or clicks cancel
         break
-window.close()
+    elif event == 'Submit':
+        window.close()
+    elif event == 'Cancel':
+        window.close()
+
+# the input is the player's IGN as a string
 player_name = values[0]
 
-player_id_response_na = my_api.players(
-my_api.region.NA, player_name, fields='account_id', limit=1)
-player_id_na = player_id_response_na['data'][0]['account_id']
-na_data = my_api.player_personal_data(my_api.region.NA,player_id_na,fields='nickname')
-player_name_na = na_data['data'][f'{player_id_na}']['nickname']
-player_id_response_eu = my_api.players(
-my_api.region.EU, player_name, fields='account_id', limit=1)
-player_id_eu = player_id_response_eu['data'][0]['account_id']
-eu_data = my_api.player_personal_data(my_api.region.EU,player_id_eu,fields='nickname')
-player_name_eu = eu_data['data'][f'{player_id_eu}']['nickname']
-#player_name_eu = my_api.player_personal_data(my_api.region.EU,player_id_eu,fields='nickname')
+@lru_cache(maxsize=None)
+def get_player_id(region: str, player_name: str) -> int:
+    """
+    Given a player name, returns the account ID of the player in the specified region.
 
-layout = [  [sg.Text(f'NA:    {player_name_na}:     {player_id_na}')], [sg.B('Copy NA ID to clipboard')],
-            [sg.Text(f'EU:    {player_name_eu}:     {player_id_eu}')], [sg.B('Copy EU ID to clipboard')],
-            [sg.Button('Close Window')]]  # identify the multiline via key option
+    :param region: str, the region of the player
+    :param player_name: str, the name of the player
+    :return: int, the account ID of the player
+    """
+    # Call the API to get the player's account ID
+    player_id_response = my_api.players(region, player_name, fields='account_id', limit=1)
+
+    # Extract the account ID from the response data
+    player_id = player_id_response['data'][0]['account_id']
+
+    return player_id
+
+
+
+
+  
+@lru_cache(maxsize=None)
+def get_player_name(region: str, player_id: int) -> str:
+    """
+    Given a region and a player ID, this function retrieves the player's nickname and returns it as a string.
+
+    Args:
+        region (str): A string representing the region of the player.
+        player_id (int): An integer representing the ID of the player.
+
+    Returns:
+        str: A string representing the player's nickname.
+    """
+    # Query the API for the player's nickname using their region and player ID
+    data = my_api.player_personal_data(region, player_id, fields='nickname')
+
+    # Extract the player's nickname from the response
+    player_name = data['data'][str(player_id)]['nickname']
+
+    # Return the player's nickname
+    return player_name
+
+
+# searches the API for the nickname, 
+# returning the player ID & nickname 
+# of the player that most closely matches the search query
+player_id_response_na = my_api.players(
+NA, player_name, fields='account_id', limit=1)
+player_id_na = get_player_id(NA, player_name)
+player_name_na = get_player_name(NA, player_id_na)
+
+# searches the API for the nickname, 
+# returning the player ID & nickname 
+# of the player that most closely matches the search query
+player_id_response_eu = my_api.players(
+EU, player_name, fields='account_id', limit=1)
+player_id_eu = get_player_id(EU, player_name)
+player_name_eu = get_player_name(EU, player_id_eu)
+
+#  setup a layout for the GUI, showing the player's name, id, and a button to copy the id to the clipboard.
+layout = [  [sg.T(f'NA:    {player_name_na}:     {player_id_na}')], [sg.B('Copy NA ID to clipboard')],
+            [sg.T(f'EU:    {player_name_eu}:     {player_id_eu}')], [sg.B('Copy EU ID to clipboard')],
+            [sg.B('Close Window')]]  # Button to close window
 
 # Create the Window
 window = sg.Window('Output', layout).Finalize()
@@ -60,8 +119,8 @@ while True:
     event, values = window.read()
     if event in (None, 'Close Window'): # if user closes window or clicks cancel
         break
-    elif event == 'Copy NA ID to clipboard':
+    elif event == 'Copy NA ID to clipboard': # if the user clicks the button to copy the NA ID to the clipboard, it does just that
         pyperclip.copy(player_id_na)
-    elif event == 'Copy EU ID to clipboard':
+    elif event == 'Copy EU ID to clipboard': # if the user clicks the button to copy the EU ID to the clipboard, it does just that
         pyperclip.copy(player_id_eu)
 window.close()
